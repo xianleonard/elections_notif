56837f51c807398f0619586a19f31b2941fb15bc
Christian Leonard <xian.leonard@gmail.com>
2022-06-16 11:54:07 -0700

    initial commit

---
+++ notif_only.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(rvest)
+
+page <- read_html("https://results.lavote.gov/text-results/4269")
+
+page_tables <- page %>%
+  html_table() %>%
+  unlist(recursive=F)
+
+bad_frame <- do.call(rbind.data.frame,page_tables)
